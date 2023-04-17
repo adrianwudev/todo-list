8ccc99e95031319a79a47681af5e78d3c6f8c74e
adrian <adrian657984@gmail.com>
2023-04-17 18:40:52 +0800

    Setup Dispatcher Servlet

---
+++ src/main/java/ad/adlearn/config/WebConfig.java
@@ -0,0 +1,11 @@
+package ad.adlearn.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "ad.adlearn")
+public class WebConfig {
+}
